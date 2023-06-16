d8ce5225fad54605db807df639a317a5c0268b79
liulipeng <liulipeng@huan.tv>
2023-06-16 14:32:20 +0800

    init project

--- src/build/BuildConfig.js
+++
@@ -1,5 +0,0 @@
-export default {
-  DEBUG: true,//调试模式
-  TEST_ENV: true,//测试环境
-  ES_APP_VERSION: 1,//ES小程序版本号
-}
--- src/main-native.ts
+++ src/main-native.ts
@@ -1,12 +1,9 @@
 import {
-  createApp, EventBus,
-  HippyApp, setScreenSize,
+  createApp,
+  HippyApp,
 } from '@hippy/vue-next';
 
 import ESApp from './App.vue';
-
-import Vue from 'vue';
-
 console.log('------33333333333333333333-----------????????')
 
 const app: HippyApp = createApp(ESApp, {
@@ -23,29 +20,24 @@ setESApp(app);
 // app.use(ESComponent);
 
 
-setScreenSize({
-  width: 1920,
-  height: 1080,
-});
+// setScreenSize({
+//   width: 1920,
+//   height: 1080,
+// });
 
-import ESRouter from "./ESRouter/src/index.js";
-import routes from './routes';
 //-------------------ESRouter---------------------
-const router = new ESRouter(routes);
-console.log('------2-------initCallback-------------????????' + Vue)
-app.use(ESRouter);
-
-console.log('------3-------initCallback-------------????????')
-
+// import ESRouter from "./ESRouter/src/index.js";
+// import routes from './routes';
+// app.use(ESRouter);
 //----------------------------------------------
 const initCallback = ({superProps, rootViewId}) => {
   console.log('------1-------initCallback-------------????????')
   app.mount('#root');
-  let route = {
-    name: 'index',
-    params: {},
-  }
-  router.push(route);
+  // let route = {
+  //   name: 'index',
+  //   params: {},
+  // }
+  // router.push(route);
   console.log('------4-------initCallback-------------????????')
 };
 
